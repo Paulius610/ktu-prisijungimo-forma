fcbb59533e18064aadf5798d1f36738d18314e9e
Paulius610 <paulius610@gmail.com>
2015-03-06 20:34:38 +0200

    Duomenų bazė

--- MarGePauLuDo/db/setup.sql
+++ MarGePauLuDo/db/setup.sql
@@ -1,8 +1,8 @@
 CREATE TABLE  `setup` (
-`name` TEXT NOT NULL ,
-`username` TEXT NOT NULL ,
-`email` TEXT NOT NULL ,
-`password` TEXT NOT NULL ,
-`codegenerator` TEXT NOT NULL ,
+`name` TEXT NOT NULL,
+`username` TEXT NOT NULL,
+`email` TEXT NOT NULL,
+`password` TEXT NOT NULL,
+`codegenerator` TEXT NOT NULL,
 `id` TEXT NOT NULL
 );

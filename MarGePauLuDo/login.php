1e671439867850d556eed873a4f9946bb329dc18
Paulius610 <paulius610@gmail.com>
2015-03-06 19:30:52 +0200

    Prisijungimo forma

---
+++ MarGePauLuDo/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<head>
+<style>
+body {
+background-color: rgb(21,185,26);
+}
+</style>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <form>
+  <div class="form-group">
+    <label for="InputUsername">Username: </label>
+    <input type="email" class="form-control" id="InputEmail" placeholder="Enter username">
+  </div>
+  <div class="form-group">
+    <label for="InputPassword">Password: </label>
+    <input type="password" class="form-control" id="InputPassword" placeholder="Password">
+  </div>
+  <button type="submit" class="btn btn-default">Login</button>
+</form>
+</body>
+</html>

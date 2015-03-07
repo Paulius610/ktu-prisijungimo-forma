f0e701bd5bc564045a8a64bdaa19beb920c81a98
JustasMork <justas.mork@gmail.com>
2015-03-07 22:21:52 +0200

    Prisijungimo forma su bootsrap

---
+++ JM/Prisijungimas.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html"><head lang="en">
+    <meta charset="UTF-8">
+    <title>JM</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="row" align="center">
+
+
+    <h1><strong>Kažkoks LOGO</strong></h1>
+    <br>
+
+    <table class="table table-bordered">
+
+    <td class="info" align="right">
+        <div class="col-md-12" align="right">
+
+
+        <h2>Prisijungti</h2>
+        <br>
+        <a>Vardas</a><br>
+        <input type="text" name="Vardas" placeholder="Petras"><br>
+        <br>
+        <a>Slaptažodis</a><br>
+        <input type="password" name="Slaptažodis" placeholder="Petraitis"><br>
+        <br>
+        <button class="btn btn-primary" name="Prisijungti">Prisijungti</button><br>
+        </div>
+        </td>
+
+    <td class="success">
+
+    <div class="col-md-12" align="left">
+
+        <h2 >Registruotis</h2><br>
+        <a>Vardas</a><br>
+        <input type="text" name="Vardas" placeholder="Petras"><br>
+        <br>
+        <a>Slaptažodis</a><br>
+        <input type="password" name="Slaptažodis" placeholder="Petraitis"><br>
+        <br>
+        <a>El.paštas</a><br>
+        <input type="text" name="El.paštas" placeholder="P.Petraitis@gmail.com"><br>
+        <br>
+        <button class="btn btn-success" name="Registruotis">Registruotis</button>
+        </td>
+
+    </table>
+
+    <button class="btn btn-danger">Pamiršote slaptažodį?</button>
+
+    </div>
+
+</div>
+
+
+
+
+
+</body></html>

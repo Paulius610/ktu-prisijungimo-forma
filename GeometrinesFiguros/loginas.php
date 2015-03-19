795a90b5fab098fe71c9a8e1bd253139ff1ad043
AdamsLT <surmalas@gmail.com>
2015-03-19 14:20:15 +0200

    Sukurta PHP prisijungimo dalis. Neveikia.
    HTML faile atsirado PHP dalis kažkodėl.

--- GeometrinesFiguros/loginas.php
+++ GeometrinesFiguros/loginas.php
@@ -11,37 +11,40 @@
 <?php
 $host = 'mysql9.000webhost.com';
 $username = 'a4119726_admin';
-$password = 'lopaslt2';
-$database = 'a4119726_db';
+$password = 'slaptazodis1';
+$database = 'a4119726_persons';
 
 $self = $_SERVER['PHP_SELF'];
 $ipaddress = ("$_SERVER[REMOTE_ADDR]");
 
-$connect = mysql_connect($host,$username,$password) or die('Unable to connect to the database server at this time.');
+$connect = mysql_connect($host,$username,$password) or die('<p class="error">Unable to connect to the database server at this time.</p>');
 
-mysql_select_db($database,$connect) or die('Unable to connect to the database at this time.');
+mysql_select_db($database,$connect) or die('<p class="error">Unable to connect to the database at this time.</p>');
 
-    if(isset($_POST['send'])) {
-        if(empty($_POST['email']) || empty($_POST['password']) )) {
-            echo('Nesuvedėte visų reikiamų duomenų');
-        }
+if(isset($_POST['submit'])) {
+    if(empty($_POST['email']) || empty($_POST['password']) ) {
+        echo('Nesuvedėte visų reikiamų duomenų');
+    }
     else {
         $email = htmlspecialchars(mysql_real_escape_string($_POST['email']));
-        $password = htmlspecialchars(mysql_real_escape_string($_POST['post']));
+        $password = htmlspecialchars(mysql_real_escape_string($_POST['password']));
 
-$sql = "SELECT FROM db WHERE email='$email', password='$password';";
+        $sql = "SELECT email, password FROM WHERE email='$email', password='$password';";
+        $query = @mysql_query($sql);
+        $rows = mysql_num_rows($query);
 
-    if (@mysql_query($sql)) {
-        echo('<p>Prisijungėte.</p>');
-    }
 
-    else {
-        echo('Suklydote vesdami el. paštą arba/ir slaptažodį.');
-    }
+        if ($rows==1) {
+            echo("valio");
+            header("Location: http://google.com");
+        }
+
+        else {
+            header("Location: http://google.com");
+        }
     }
 }
 ?>
-
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <div class="container">
     <div class="row vertical-offset-100">

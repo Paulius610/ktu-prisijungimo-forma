feb3410669611d6d98a105a93302332bb0bc88d3
AdamsLT <surmalas@gmail.com>
2015-03-19 14:35:26 +0200

    php kodas inkliudintas į html kodą, kad išlaikyt panašumą tarp failų

--- GeometrinesFiguros/loginas.php
+++ GeometrinesFiguros/loginas.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-</head>
-
-<body>
-
 <?php
 $host = 'mysql9.000webhost.com';
 $username = 'a4119726_admin';
@@ -45,32 +35,3 @@ if(isset($_POST['submit'])) {
     }
 }
 ?>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<div class="container">
-    <div class="row vertical-offset-100">
-        <div class="col-md-4 col-md-offset-4">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Prašome prisijungti</h3>
-                </div>
-                <div class="panel-body">
-                    <form accept-charset="UTF-8" role="form" action="<?php $self ?>" method="post">
-                        <fieldset>
-                            <div class="form-group">
-                                <input class="form-control" placeholder="El. paštas" name="email" type="text">
-                            </div>
-                            <div class="form-group">
-                                <input class="form-control" placeholder="Slaptažodis" name="password" type="password" value="">
-                            </div>
-                            <button type="button" class="btn btn-link"><a href="priminimas.html">Parmišau duomenis</a></button>
-                            <input class="btn btn-lg btn-success btn-block" type="submit" value="Prisijungti">
-
-                            <a class="btn btn-lg btn-primary btn-block" href="registracija.html" role="button">Registracija</a>
-                        </fieldset>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-</body>

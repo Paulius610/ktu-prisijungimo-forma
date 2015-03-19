028545242689319918fee01b8be2caf2a821428a
DomWake <domas.lavr@gmail.com>
2015-03-19 14:44:31 +0200

    priminimo php super alpha versija

--- GeometrinesFiguros/priminimas.php
+++ GeometrinesFiguros/priminimas.php
@@ -1,7 +1,32 @@
 <?php
-$host = 'mysql3.000webhost.com'; //usually localhost
-$username = 'a1952851_a195285'; //your username assigned to your database
-$password = 'slaptazodis1'; //your password assigned to your user & database
-$database = 'db'; //your database name
+$host = 'mysql3.000webhost.com';
+$username = 'a1952851_a195285';
+$password = 'slaptazodis1';
+$database = 'db';
 
+$self = $_SERVER['PHP_SELF'];
+$ipaddress = ("$_SERVER[REMOTE_ADDR]");
+
+$connect = mysql_connect($host,$username,$password) or die('<p class="error">Unable to connect to the database server at this time.</p>');
+
+mysql_select_db($database,$connect) or die('<p class="error">Unable to connect to the database at this time.</p>');
+
+if(isset($_POST['submit'])) {
+    if(empty($_POST['email']){
+        echo('Nesuvedėte visų reikiamų duomenų');
+    }
+    else {
+        $email = htmlspecialchars(mysql_real_escape_string($_POST['email']));
+
+        $sql = "SELECT email FROM WHERE email='$email';";
+
+        if ($rows==1) {
+            echo("Priminimas dėl slaptažodžio išsiųstas");
+        }
+
+        else {
+            echo("Toks el. paštas neegzistuoja");
+        }
+    }
+}
 ?>

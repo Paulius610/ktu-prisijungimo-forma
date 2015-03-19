3c431a4c63928cb0130202a1c062c646e26792bb
DomWake <domas.lavr@gmail.com>
2015-03-19 14:18:26 +0200

    priminimo php prijungimas i duom.baze

--- GeometrinesFiguros/priminimas.php
+++ GeometrinesFiguros/priminimas.php
@@ -1,1 +1,7 @@
-placeholder
+<?php
+$host = 'mysql3.000webhost.com'; //usually localhost
+$username = 'a1952851_a195285'; //your username assigned to your database
+$password = 'slaptazodis1'; //your password assigned to your user & database
+$database = 'db'; //your database name
+
+?>
